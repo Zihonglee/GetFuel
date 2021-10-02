75fcd0e168652b3c09d6bdd5cf365f7022dc30b6
cheehau <cheehau@iastate.edu>
2021-10-01 21:30:17 -0500

    all db

--- Experiments/Cheehau/demo1/src/main/java/com/example/demo1/TestData.java
+++ Experiments/Cheehau/demo1/src/main/java/com/example/demo1/TestData.java
@@ -1,2 +1,18 @@
-package com.example.demo1;public class TestData {
+package com.example.demo1;
+
+public class TestData {
+
+    private String message;
+
+    public TestData() {}
+
+    public TestData(String message) {
+
+        this.message = message;
+    }
+
+    public String getMessage() {
+
+        return message;
+    }
 }
