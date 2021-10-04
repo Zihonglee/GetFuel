fe1863713982d7dab8ad239f0bb5d6895c591455
Andrea Gameros <agameros@iastate.edu>
2021-10-04 07:50:22 -0700

    testing add

--- Experiments/Andrea Gameros/GetFueledRestaurantPage/app/src/main/java/com/example/getfueledrestaurantpage/CreateAccountPage.java
+++ Experiments/Andrea Gameros/GetFueledRestaurantPage/app/src/main/java/com/example/getfueledrestaurantpage/CreateAccountPage.java
@@ -7,14 +7,23 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class CreateAccountPage extends AppCompatActivity
@@ -24,6 +33,7 @@ public class CreateAccountPage extends AppCompatActivity
     private EditText Password2;
     private Button CreateAccount;
     private Button SigninHere;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,8 +53,9 @@ public class CreateAccountPage extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Intent newIntent = new Intent(view.getContext(), LoginPage.class);
-                startActivity(newIntent);
+                //Intent newIntent = new Intent(view.getContext(), LoginPage.class);
+                //startActivity(newIntent);
+                createAccount(Email.getText().toString(), Password.getText().toString(), Password2.getText().toString());
             }
         });
 
@@ -59,27 +70,60 @@ public class CreateAccountPage extends AppCompatActivity
         });
     }
 
-    private void createAccount(String email, String password)
+    private void createAccount(String email, String password, String password2)
     {
-
+        //sends alert
+        if(checkPassword(password, password2) == false)
+        {
+            Toast.makeText(CreateAccountPage.this,"Passwords don't match", Toast.LENGTH_LONG).show();
+        }
     }
 
-    private void checkPassword(String password, String password2)
+    private boolean checkPassword(String password, String password2)
     {
         //then store all information to database
         if(password.equals(password2))
         {
-            int t = 2;
+            return true;
         }
         //send error message
-        else
-        {
-
-        }
+        return false;
     }
 
-    private void alert(String message)
+    private void PostRequest()
     {
-       // AlertDialog dlg = new AlertDialogueBuilder(CreateAccountPage.this)
+        String url = "https://d94fb843-c270-4ed7-a3b3-640a9e0f4f0a.mock.pstmn.io/userIDs"; //url for info
+        //creates string request
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, null,
+                new Response.Listener<JSONArray>()
+                {
+                    @Override
+                    public void onResponse(JSONArray response)
+                    {
+                        Toast.makeText(CreateAccountPage.this, "Account created", Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(CreateAccountPage.this, "Failed to create account", Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            //code to execute, add parameters to request
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", Email.getText().toString());
+                params.put("password", Password.getText().toString());
+
+                return params;
+            }
+        };
+
+        requestQueue = Volley.newRequestQueue(CreateAccountPage.this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
--- Experiments/Andrea Gameros/GetFueledRestaurantPage/app/src/main/java/com/example/getfueledrestaurantpage/LoginPage.java
+++ Experiments/Andrea Gameros/GetFueledRestaurantPage/app/src/main/java/com/example/getfueledrestaurantpage/LoginPage.java
@@ -9,6 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class LoginPage extends AppCompatActivity
 {
@@ -18,6 +29,8 @@ public class LoginPage extends AppCompatActivity
     private TextView Info;
     private int counter = 3; //number of login attempts
     private Button CreateAccount;
+    private RequestQueue requestQueue;
+    private TextView msgResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +43,7 @@ public class LoginPage extends AppCompatActivity
         Login = (Button)findViewById(R.id.btnLogin);
         Info = (TextView)findViewById(R.id.tvInfo);
         CreateAccount = (Button)findViewById(R.id.btnCreateAccount);
+        msgResponse = (TextView)findViewById(R.id.msgResponse);
 
         Info.setText("Number of attempts remaining: 3");
 
@@ -38,7 +52,8 @@ public class LoginPage extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                checkInfo(Name.getText().toString(), Password.getText().toString());
+                //checkInfo(Name.getText().toString(), Password.getText().toString());
+                jsonParse(Name.getText().toString(), Password.getText().toString());
             }
         });
 
@@ -54,6 +69,56 @@ public class LoginPage extends AppCompatActivity
         });
     }
 
+    //parses through ids
+    private void jsonParse(String email, String password)
+    {
+        String url = "https://d94fb843-c270-4ed7-a3b3-640a9e0f4f0a.mock.pstmn.io/userIDs";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                try
+                {
+                    JSONArray jsonArray = response.getJSONArray("users");
+
+                    for (int i = 0; i < jsonArray.length(); i++)
+                    {
+                        JSONObject users = jsonArray.getJSONObject(i);
+                        String emailJ = users.getString("email");
+                        String passwordJ = users.getString("password");
+
+                        if(email == emailJ && password == passwordJ)
+                        {
+
+                            //Intent intent = new Intent(LoginPage.this, HomePage.class);
+                            //startActivity(intent);
+                            msgResponse.append(emailJ + " " + passwordJ);
+                            System.out.println(msgResponse.toString());
+                        }
+
+                    }
+                } catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(request);
+
+    }
+
+    /**
     //need to set this to check database of usernames and passwords
     private void checkInfo(String userName, String userPassword)
     {
@@ -78,4 +143,5 @@ public class LoginPage extends AppCompatActivity
             }
         }
     }
+     **/
 }
