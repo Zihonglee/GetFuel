b90d0a14ec7f2f1da1b60daee6e93b01fa865e56
Jayson Lee <jaysonlee@jaysonlee.student.iastate.edu>
2021-10-08 16:01:48 -0500

    Search bar and list

--- Experiments/Jayson Lee/volleyTestAgain/app/src/main/java/com/example/jsontest/SearchPage.java
+++ Experiments/Jayson Lee/volleyTestAgain/app/src/main/java/com/example/jsontest/SearchPage.java
@@ -1,14 +1,47 @@
 package com.example.jsontest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SearchPage extends AppCompatActivity {
+
+    private SearchView searchView;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    String[] list = {"Jayson", "Benedict", "Vincent", "Chee Hau", "Andy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+
+        searchView = findViewById(R.id.searchBar);
+        listView = findViewById(R.id.listName);
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                SearchPage.this.arrayAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                SearchPage.this.arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
     }
 }
