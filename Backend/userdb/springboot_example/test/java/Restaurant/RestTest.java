190345789b939cd3ecfc5fa3dbdb3aff09ff1260
Lee <vincet@iastate.edu>
2021-10-24 03:19:20 -0500

    add

--- Backend/userdb/springboot_example/test/java/Restaurant/RestTest.java
+++ Backend/userdb/springboot_example/test/java/Restaurant/RestTest.java
@@ -1,9 +1,10 @@
 package Restaurant;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -44,26 +45,44 @@ public class RestTest
 		assertEquals("https://www.google.com/search?client=firefox-b-1-d&q=pizza", rest.getUrl());
 		
 		verify(repo, times(2)).getRestaurantById(Long.valueOf(1));
+		verify(repo, times(2)).getRestaurantById(anyLong());
+	}
+	
+	@Test
+	public void deleteRestaurantTest()
+	{
+		verify(repo, never()).deleteRestaurantById(anyLong());
+		when(repo.getRestaurantById(Long.valueOf(1))).thenReturn(new Restaurant("Pizza", "$10.00", "7.5", new Cuisine("Japanese"), "https://www.google.com/search?client=firefox-b-1-d&q=pizza"));
+		
+		doNothing().when(repo).deleteRestaurantById(Long.valueOf(1));
+		String rest = restService.deleteRestaurantById(Long.valueOf(1));
+		
+		assertEquals("Restaurant deleted", rest);
+		
+		verify(repo, atMost(1)).getRestaurantById(anyLong()); //since there is only one line of getrestaurantbyid that i mocked
+		verify(repo, atMost(1)).deleteRestaurantById(anyLong());
 	}
 
-//	@Test
-//	public void getAllRestaurantTest() 
-//	{
-//		List<Restaurant> list = new ArrayList<Restaurant>();
-//		Cuisine cs = new Cuisine("Asian");
-//		Restaurant rest1 = new Restaurant("US Pizza", "$7.00", "8.5", cs, "https://uspizzaco.net/");
-//		Restaurant rest2 = new Restaurant("sze chuan", "$11.00", "9.0", cs, "https://www.szechuanhouseames.com/");
-//		Restaurant rest3 = new Restaurant("potbelly", "$6.00", "8.0", cs, "https://www.potbelly.com/");
-//
-//		list.add(rest1);
-//		list.add(rest2);
-//		list.add(rest3);
-//
-//		when(restService.getAllRestaurant()).thenReturn(list);
-//
-//		List<Restaurant> acctList = restService.getAllRestaurant();
-//
-//		assertEquals(3, acctList.size());
-//	}
+	@Test
+	public void getAllRestaurantTest() 
+	{
+		List<Restaurant> list = new ArrayList<Restaurant>();
+		Cuisine cs = new Cuisine("Asian");
+		Restaurant rest1 = new Restaurant("US Pizza", "$7.00", "8.5", cs, "https://uspizzaco.net/");
+		Restaurant rest2 = new Restaurant("sze chuan", "$11.00", "9.0", cs, "https://www.szechuanhouseames.com/");
+		Restaurant rest3 = new Restaurant("potbelly", "$6.00", "8.0", cs, "https://www.potbelly.com/");
+
+		list.add(rest1);
+		list.add(rest2);
+		list.add(rest3);
+
+		when(restService.getAllRestaurant()).thenReturn(list);
+
+		List<Restaurant> restList = restService.getAllRestaurant();
+
+		assertEquals(3, restList.size());
+		
+		verify(repo, never()).getRestaurantById(anyLong());
+	}
 
 }
