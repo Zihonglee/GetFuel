72f4c1b9b7979599e14696ca42f0a0a447808dd1
cheehau <cheehau@iastate.edu>
2021-10-27 21:35:32 -0500

    Update RoleRepository.java

--- Backend/userdb/springboot_example/src/main/java/onetoone/Roles/RoleRepository.java
+++ Backend/userdb/springboot_example/src/main/java/onetoone/Roles/RoleRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //package onetoone.Roles;
 //
 //import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,20 +14,3 @@
 //    public void deleteRoleById(Long id);
 //
 //}
-=======
-package onetoone.Roles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Repository
-public interface RoleRepository  extends JpaRepository <Role, Long>
-{
-    public Role getRoleById(Long id);
-
-    @Transactional
-    public void deleteRoleById(Long id);
-
-}
->>>>>>> 137d015e2fe519f21b1f3ce63c21e551f91d1864
