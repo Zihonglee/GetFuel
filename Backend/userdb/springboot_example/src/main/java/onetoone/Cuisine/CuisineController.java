e220e8554e2daa409a3bd031858b93494874efa8
Lee <vincet@iastate.edu>
2021-10-29 23:45:55 -0500

    sorting part

--- Backend/userdb/springboot_example/src/main/java/onetoone/Cuisine/CuisineController.java
+++ Backend/userdb/springboot_example/src/main/java/onetoone/Cuisine/CuisineController.java
@@ -3,15 +3,8 @@ package onetoone.Cuisine;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/cuisines")
@@ -23,7 +16,7 @@ public class CuisineController
 	@GetMapping
 	public List<Cuisine> getAllCuisine()
 	{
-		return cuisineRepository.findAll();
+		return cuisineRepository.findAll(Sort.by(Sort.Direction.ASC, "cuisine_type"));
 	}
 
 	@GetMapping("{id}")
