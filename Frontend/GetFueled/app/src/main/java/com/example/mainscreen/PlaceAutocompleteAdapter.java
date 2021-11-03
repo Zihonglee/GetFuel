d6b35a94560198b1f6f1f70e5bf4e633527d1cb2
Andrea Gameros <agameros@iastate.edu>
2021-11-03 12:03:39 -0500

    map changes

---
+++ Frontend/GetFueled/app/src/main/java/com/example/mainscreen/PlaceAutocompleteAdapter.java
@@ -0,0 +1,9 @@
+package com.example.mainscreen;
+
+import com.google.android.libraries.places.compat.AutocompleteFilter;
+
+public class PlaceAutocompleteAdapter
+{
+    //this class will generate auto-complete for search bar in maps
+    //uses google places api but it is currently out-dated so will worry about/cry about later
+}
