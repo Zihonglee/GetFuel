fa2ded30d31155071ccc3bf0863e57bb9c3c6149
cheehau <cheehau@iastate.edu>
2021-11-17 23:27:42 -0600

    websocket

---
+++ Backend/userdb/springboot_example/src/main/java/onetoone/WebSocket/Websocket.java
@@ -0,0 +1,22 @@
+package onetoone.WebSocket;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@ServerEndpoint("/websocket/{user}")
+@Component
+public class Websocket {
+}
