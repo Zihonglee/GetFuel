a7b64171e9394cb3ba41c36c474ee4d3ab4faedd
cheehau <cheehau@iastate.edu>
2021-11-21 03:58:43 -0600

    update websocket for user

--- Backend/userdb/springboot_example/src/main/java/onetoone/WebSocket/Websocket.java
+++ Backend/userdb/springboot_example/src/main/java/onetoone/WebSocket/Websocket.java
@@ -3,6 +3,8 @@ package onetoone.WebSocket;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.Scanner;
+
 import onetoone.Users.User;
 import onetoone.Users.UserRepository;
 
@@ -16,32 +18,55 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
 
-@ServerEndpoint("/websocket/{user}")
+@ServerEndpoint("/websocket/{userid}")
 @Component
 public class Websocket {
 
     private final Logger logger1 = LoggerFactory.getLogger(Websocket.class);
-    UserRepository userRepository;
+    private static UserRepository userRepository;
+    private LocalDateTime timeCreated = LocalDateTime.now();
 
+    @Autowired
+    public void setUserRepository(UserRepository repo){
+        userRepository = repo
+    }
 
-    private static Map<Session,Long> sessionIdMap = new Hashtable<>();
-    private static Map<Long,Session> idSessionMap = new Hashtable<>();
+    private static Map<Session,User> sessionIdMap = new Hashtable<>();
+    private static Map<User,Session> idSessionMap = new Hashtable<>();
 
     private final Logger logger = logger1;
 
+    private void broadcast(String message) {
+        sessionIdMap.forEach((session, user) -> {
+            try {
+                session.getBasicRemote().sendText(message);
+            }
+            catch (IOException e) {
+                logger.info("Exception: " + e.getMessage().toString());
+                e.printStackTrace();
+            }
+
+        });
+
+    }
+
 
     @OnOpen
-    public void onOpen(Session session,@PathParam("username") String username ) {
+    public void onOpen(Session session,@PathParam("userid") Long userid ) {
         try {
 
 logger.info("User connected");
-User user = userRepository.findByName(username);
+User user = userRepository.getUserById(userid);
 
-            sessionIdMap.put(session,user.getId());
-            idSessionMap.put(user.getId(),session);
+            sessionIdMap.put(session,user);
+            idSessionMap.put(user,session);
 
+            String message = "User:" +user.getName()+"open the application";
+            broadcast(message);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,12 +74,44 @@ User user = userRepository.findByName(username);
     }
 
     @OnMessage
-    public void onMessage(Session session){
-//update all data ?
+    public void onMessage(Session session, String UserInfo){
+logger.info("Enter into Message: Got Message:"+ UserInfo);
+User user = sessionIdMap.get(session);
+Scanner sc = new Scanner(UserInfo);
+String[] list = sc.delimiter().split(",");
+boolean userexist = false;
+
+for(int i = 0; i< userRepository.findAll().size(); i++){
+    if(userRepository.findAll().get(i).getId().equals(list[0])){
+        userexist= true;
+    }
+}
+if(userexist) {
+    Long id = Long.parseLong(list[0]);
+    User edituser = userRepository.getUserById(id);
+    edituser.setName(list[1]);
+    edituser.setEmail(list[2]);
+    edituser.setPassword(list[3]);
+    edituser.setRoleType(list[4]);
+    userRepository.save(edituser);
+}else {
+    User newuser = new User(list[1],list[2],list[3],list[4]);
+    userRepository.save(newuser);
+}
+
     }
 
     @OnClose
     public void onClose(Session session){
+        logger.info("Entered into Close");
+
+        User user = sessionIdMap.get(session);
+        sessionIdMap.remove(session);
+        idSessionMap.remove(user);
+
+        // broadcase that the user disconnected
+        String message = user + " disconnected";
+        broadcast(message);
 
     }
 
